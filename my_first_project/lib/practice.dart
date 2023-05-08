2de91e8a6275066cc63befc14100188d6bb74b8b
hridoy <hridoyhk056@gmail.com>
2023-05-08 15:00:30 +0600

    container

--- my_first_project/lib/practice.dart
+++ my_first_project/lib/practice.dart
@@ -1,11 +1,56 @@
-import 'dart:io';
 
 void main(){
-  
-  print("Hello World !");
-  stdout.write("Enter Your Name:");
-  var name = stdin.readLineSync();
-  print("Welcome, $name");
+  print('Welcome..');
+  // print("Hello World !");
+  // stdout.write("Enter Your Name:");
+  // var name = stdin.readLineSync();
+  // print("Welcome, $name");
+  //
+  // Human(); //creating a class object;
+  //
+  // //Declaration of variable
+  // int a ;
+  // // assign
+  // a = 5;
+  // print(a);
+  // a = 7;
+  // print(a);
+  // //inline declaration
+  // String b = "Hridoy";
+  // BigInt longValue;
+  // longValue = BigInt.parse('8389302000273737237');
+  // print(longValue);
+  //
+  // num  parcentage = 99.45;
+  //
+  // bool isLogin = false;
+  // isLogin = true;
 
-  
+  // String name = "Raman";
+  // //var
+  // var subject = "Math";
+  // var rollNo = 11;
+  // var section;
+  // section = "D";
+  // section = 7;
+  // dynamic sec;
+  // sec = "D";
+  // sec = 7;
+
+  var myclass = myClass();
+  myclass.myFunc();
+
+}
+// class Human{
+//
+//   Human();
+//
+// }
+
+class myClass{
+
+  void myFunc(){
+    print('Hridoy');
+  }
+
 }
