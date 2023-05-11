81a4413f421522de682cf779d38af017d61a6ec6
hridoy <hridoyhk056@gmail.com>
2023-05-12 00:31:21 +0600

    ListView

--- listview/lib/main.dart
+++ listview/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -32,6 +33,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  var arrnames = ["Hridoy","HK","Bulbul","Ahamad","Bokul"];
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +43,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Text("Hello World"), // This trailing comma makes auto-formatting nicer for build methods.
+      body:ListView.separated(itemBuilder: (context, index) {
+        return Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(arrnames[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w700),),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(arrnames[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w700),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(arrnames[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w700),),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+        itemCount: arrnames.length,
+        separatorBuilder: (context , index){
+        return Divider(height: 100, thickness: 1,);
+        },
+      ),
+      // ListView.builder(itemBuilder: (context, index) {
+      //   return Text(arrnames[index],style: TextStyle(fontWeight: FontWeight.w500,fontSize: 21),);
+      // },
+      //   itemCount: arrnames.length,
+      //   itemExtent: 100,
+      //   scrollDirection: Axis.horizontal,
+      // ),
+      // ListView(
+      //   reverse: true,
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("One",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Two",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Three",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Four",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Five",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Six",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Text("Seven",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      //     ),
+      //   ],
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
