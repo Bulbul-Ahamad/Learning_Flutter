dc53e5becb30ac5f5d1d5637b38326c75f5d24f9
hridoy <hridoyhk056@gmail.com>
2023-06-04 19:38:52 +0600

    Learning Dart

--- main.dart
+++ main.dart
@@ -1,47 +1,200 @@
 void main() {
-  int age = 30;
-  print(age);
-  double a = 2.4444;
-  print(a);
-  String name = 'Hriody';
-  print(name);
-  bool value = true;
-  print(value);
+  //for loop
+//   for(var i=0;i<10;i++){
+//     if(i==5){
+// //       break;
+//       continue;
+//     }
+//     print(i);
+//   }
+  //for in
+//   List myList = ['a','b','c'];
+//   for (var element in myList){
+//     print(element);
+//   }
+  //for each
+// var myList = [
+//   {'n':'a'},
+//   {'n':'b'},
+//   {'n':'d'},
+// ];
+//   myList.forEach((element){
+//     print(element['n']);
+//   });
 
-  //list
-  List mylist = ['one', 'two', 'three', 'four'];
-  print(mylist);
+  //how to define
+//   addTwoNumb(){
+//     print(10+20);
+//   }
+//   addTwoNumb();
+  //arrow func
+//   arrowFunction()=>print("this is my arrow Func");
+//   arrowFunction();
+  //return
+//   example(){
+//     return 10+20;
+//   }
+//   print(example());
+  //parameter
+//   addTwoNumb(int num1,int num2){
+//     print(num1+num2);
+//   }
+//   addTwoNumb(5,5);
+//   addTwoNumb(52,52);
+//   addTwoNumb(53,53);
+  //optional positional para
+//   myFunc(a,b,c,[d,e]){
+//     print(a);
+//     print(b);
+//     print(c);
+//     print(d);
+//     print(e);
+//   }
+//   myFunc(10,20,30,5,4);
+  //optional named para
+//     myFunc(a,b,c,{d,e}){
+//     print(a);
+//     print(b);
+//     print(c);
+//     print(d);
+//     print(e);
+//   }
+//   myFunc(10,20,30,e:4,d:3);
+  //higher order func
+//   addTwoNumb(){
+//   print(10+20);
+//   }
+//   mainFunc(addTwoNumb);
 
-  //Map
-  Map<String, dynamic> myMap = {
-    'name': 'Hridoy',
-    'age': 30,
-  };
-  print(myMap);
-  //set
-  var mySet = {'one', 'two', 'three', 'four'};
-  print(mySet);
+//   final gender = Gender.female;
+//   switch(gender){
+//     case Gender.male:
+//       print('Gender is Male');
+//       break;
+//       case Gender.female:
+//       print('Gender is female');
+//       break;
+//       case Gender.unknown:
+//       print('Gender is unknown');
+//       break;
+//     default:
+//       print("nothing matched");
+//   };
 
-  //rune
-  final name2 = 'hridoy';
-  print(name2.codeUnits);
+//   List<String> myList = ['A','B','C','D'];
+//   print(myList);
+// //   myList[1] = 'BB';
+// //   print(myList);
+//   myList.replaceRange(0,2,['AA','BB','CC','DD']);
+//   print(myList);
 
-  Runes input = Runes('\u{1F49B}');
-  print(String.fromCharCodes(input));
+//   print(myList.length);
+//   print(myList[2]);
+//   myList.add('E');
+//   print(myList);
+//   myList.addAll(['F','G','H']);
+//   print(myList);
+//   //insert
+//   myList.insert(0,'AA');
+//   print(myList);
 
-  var results = name is String;
-  print(name);
+//   int a = 10;
+//   int b = 20;
+//   final age = a;
+//   const pi = b;
 
-  int? age2;
-  var result2 = age2 ?? 25;
-  print(result2);
+//   int a = 10;
+//   int b = 0;
+//   var result = a~/b;
+//   String name = "hridoy";
+//   print(result);
 
-  String color = 'red';
-  var result1 = color == 'red' ? 'color is red' : 'unknown';
-  print(result1);
+//   String a = 'we ';
+//   String b = 'are ';
+//   String c = 'learning ';
+//   String d = 'flutter';
+//   String e = 'we ' 'are ' 'learning ' 'flutter ';
+//   //1st step
+//   print(a+b+c+d);
+//   //2nd step
+//   print("$a$b$c$d");
+//   //3rd step
+//   print(e);
+//   //4th Step
+//   List<String> f = ['we','are','learning','flutter'];
+//   print(f.join(" "));
 
-  int c = 10;
-  int b = 3;
-  var result = c ~/ b;
-  print(result);
+//   String Connection = 'waitinng';
+//   if (Connection == 'connected') {
+//     print("connected");
+//   } else if (Connection == 'waitinng') {
+//     print('waiting');
+//   } else {
+//     print("disconnected");
+//   }
+
+//   switch(Connection){
+//     case 'connected':
+//       print('connected');
+//       break;
+//     case 'waitinng':
+//       print('Waiting');
+//       break;
+//     default:
+//       print('disconnected');
+//   }
+
+  // int age = 30;
+  // print(age);
+  // double a = 2.4444;
+  // print(a);
+  // String name = 'Hriody';
+  // print(name);
+  // bool value = true;
+  // print(value);
+
+  // //list
+  // List mylist = ['one', 'two', 'three', 'four'];
+  // print(mylist);
+
+  // //Map
+  // Map<String, dynamic> myMap = {
+  //   'name': 'Hridoy',
+  //   'age': 30,
+  // };
+  // print(myMap);
+  // //set
+  // var mySet = {'one', 'two', 'three', 'four'};
+  // print(mySet);
+
+  // //rune
+  // final name2 = 'hridoy';
+  // print(name2.codeUnits);
+
+  // Runes input = Runes('\u{1F49B}');
+  // print(String.fromCharCodes(input));
+
+  // var results = name is String;
+  // print(name);
+
+  // int? age2;
+  // var result2 = age2 ?? 25;
+  // print(result2);
+
+  // String color = 'red';
+  // var result1 = color == 'red' ? 'color is red' : 'unknown';
+  // print(result1);
+
+  // int c = 10;
+  // int b = 3;
+  // var result = c ~/ b;
+  // print(result);
 }
+// enum Gender{
+//   male,female,unknown;
+// }
+
+// mainFunc(Function function) {
+//   function();
+// //   return()=>print('returning a func');
+// }
