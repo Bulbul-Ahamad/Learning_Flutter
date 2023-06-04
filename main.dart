96ae94ca35d8602f119a2dd5a5a65601af45f588
hridoy <hridoyhk056@gmail.com>
2023-06-04 11:24:40 +0600

    Learning dart

--- main.dart
+++ main.dart
@@ -18,4 +18,30 @@ void main() {
     'age': 30,
   };
   print(myMap);
+  //set
+  var mySet = {'one', 'two', 'three', 'four'};
+  print(mySet);
+
+  //rune
+  final name2 = 'hridoy';
+  print(name2.codeUnits);
+
+  Runes input = Runes('\u{1F49B}');
+  print(String.fromCharCodes(input));
+
+  var results = name is String;
+  print(name);
+
+  int? age2;
+  var result2 = age2 ?? 25;
+  print(result2);
+
+  String color = 'red';
+  var result1 = color == 'red' ? 'color is red' : 'unknown';
+  print(result1);
+
+  int c = 10;
+  int b = 3;
+  var result = c ~/ b;
+  print(result);
 }
